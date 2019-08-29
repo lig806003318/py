9f82333b8ef9c64999b6b9370336399f5a5d7973
Administrator <admin@example.com>
2019-08-29 19:55:14 +0800

    20190912

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+
+
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
